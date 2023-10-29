fa68f9602affde50b04d98ec550e8d485dfd403f
Navemics <navemics@gmail.com>
2023-10-29 15:20:58 +0530

    Migrate local to GitHub 📝

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+// Default values work on localhost:3000 and foomedical.com
+// Replace these values with your own values for production
+export const MEDPLUM_PROJECT_ID = '9602358d-eeb0-4de8-bccf-e2438b5c9162';
+export const MEDPLUM_GOOGLE_CLIENT_ID = '943331072003-sjc6bbk6irkmkagj2iqu6f7fv4f4uli4.apps.googleusercontent.com';
+export const MEDPLUM_RECAPTCHA_SITE_KEY = '6LdrjFYoAAAAAL0jzdfiVnQJ3g-7izq1fiA0veq4';
